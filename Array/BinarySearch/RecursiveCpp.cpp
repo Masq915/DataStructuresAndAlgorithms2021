567b25cb82162d0d4e9824506acab29e093f81a1
Dushyant Pathak <dushyantpathak6789@gmail.com>
2019-10-13 09:44:20 +0530

    Binary Search in C++ - Recursive Implementation

---
+++ Array/BinarySearch/RecursiveCpp.cpp
@@ -0,0 +1,34 @@
+//The following code contains the c++ implementation of Binary Search algorithm.
+//Time Complexity: O(log n), where n is the number of elements in the array.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int BinarySearch(int A[], int N, int low, int high, int key)
+{
+    if(high >= low)
+    {
+        int mid = low + (high - low)/2;
+        if(A[mid] == key)
+            return 1;
+        else if(A[mid] < key)
+            return BinarySearch(A, N, mid + 1, high, key);
+        else
+            return BinarySearch(A, N, low, mid - 1, key);
+        
+    }  
+    return 0;
+}
+
+int main()
+{
+    int A[5] = {1, 2, 3, 4, 5};
+    int key = 6;
+    int low = 0;
+    int high = 4;
+    if(BinarySearch(A, 5, low, high, key))
+        cout<<"Element found\n";
+    else
+        cout<<"Element not found\n";
+    return 0;
+}
